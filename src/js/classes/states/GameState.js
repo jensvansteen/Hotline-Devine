58cb841c9bb4fd2df72a7481c4a0e246b611ae04
Jens Van Steen <jensvansteen@gmail.com>
2017-11-23 12:40:56 +0100

    startbutton toegevoegd

--- src/js/classes/states/GameState.js
+++ src/js/classes/states/GameState.js
@@ -1,20 +1,48 @@
-export default class GameState extends Phaser.State {
+  let button;
+
+export default class GameState extends Phaser.State {  
   init() {
     console.log(`init`);
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    
 
   }
   preload() {
     console.log(`preload`);
+    this.load.image('button', 'assets/buttons/start-button.png', 534, 134);
 
   }
   create() {
+    
+    this.stage.backgroundColor = '#182d3b';
+
+    button = this.add.button(this.world.centerX - 95, 600, 'button',this.startTheGame, this, 2, 1, 0);
+
+    button.onInputOver.add(this.over, this);
+    button.onInputOut.add(this.out, this);
+    button.inInputOut.add(this.up, this)
 
   }
-  update() {
+  update() {}
 
+  render() {}
+  
+  
+  out() {
+    console.log('die out dingens');
+  }
+  
+  over() {
+    console.log('button over');
+  }
+  
+  up() {
+    console.log('button over');
   }
 
-  render() {
-
+  startTheGame() {
+    console.log('Start game');
+    button.inputEnabled = false;
   }
 }
