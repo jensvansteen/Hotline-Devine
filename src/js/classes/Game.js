b23db2ba23942a41c422b591bd5043a8bfc71717
Nicolas Nuytten <nicolas.nuytten@student.howest.be>
2017-11-23 11:51:00 +0100

    start screen toegevoegd

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -2,7 +2,7 @@ import GameState from './states/GameState';
 
 export default class Game extends Phaser.Game {
   constructor() {
-    super(400, 400, Phaser.AUTO, `content`);
+    super(window.screen.width, window.screen.height, Phaser.AUTO, `content`);
     this.state.add('Game', GameState);
     this.state.start('Game');
   }
