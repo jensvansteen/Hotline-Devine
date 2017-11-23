c8c20c4016af721ea073c9b6cbd81880fe2f0add
Nicolas Nuytten <nicolas.nuytten@student.howest.be>
2017-11-23 12:32:50 +0100

    screen size aangepast

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -2,7 +2,7 @@ import GameState from './states/GameState';
 
 export default class Game extends Phaser.Game {
   constructor() {
-    super(window.screen.width, window.screen.height, Phaser.AUTO, `content`);
+    super(1336, 768, Phaser.AUTO, `content`);
     this.state.add('Game', GameState);
     this.state.start('Game');
   }
